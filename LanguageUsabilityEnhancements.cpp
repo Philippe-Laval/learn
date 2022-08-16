7a13fa081f0cf4a8315dc76300a7e0acce74022f
Philippe Laval <philippe.laval.1968@laposte.net>
2022-08-16 23:37:43 +0200

    Modern Cpp Tutorial

--- LanguageUsabilityEnhancements.cpp
+++ LanguageUsabilityEnhancements.cpp
@@ -601,11 +601,25 @@ enum class new_enum : unsigned int {
     value4 = 100
 };
 
+template<typename T>
+std::ostream& operator<<(
+        typename std::enable_if<std::is_enum<T>::value,
+        std::ostream>::type& stream, const T& e)
+{
+    return stream << static_cast<typename std::underlying_type<T>::type>(e);
+}
+
 int TestStronglyTypedEnumerations()
 {
     if (new_enum::value3 == new_enum::value4) { // true
         std::cout << "new_enum::value3 == new_enum::value4" << std::endl;
     }
 
+    std::cout << new_enum::value3 << std::endl;
+
     return 0;
 }
+
+
+
+
